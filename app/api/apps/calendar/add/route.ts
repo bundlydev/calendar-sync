73b893f70d9d4cd3eceaf4abc57400a9080c9031
Alan <alannnc@gmail.com>
2024-03-22 13:20:46 -0700

    fix form add more calendars

--- app/api/apps/calendar/add/route.ts
+++ app/api/apps/calendar/add/route.ts
@@ -9,27 +9,11 @@ const scopes = [
 
 export function GET() {
   const calendsyncWebsiteURL = env.CALENDAR_SYNC_WEB_URL;
-  // const clientId = env.AUTH_GOOGLE_ID;
-  // const clientSecret = env.AUTH_GOOGLE_SECRET;
+
   const redirectUri = `${calendsyncWebsiteURL}/api/integrations/googlecalendar/callback`;
-  // const oauth2Client = new google.auth.OAuth2(
-  //   clientId,
-  //   clientSecret,
-  //   redirectUri,
-  // );
 
   GoogleAuth.getClientInstance();
   const authUrl = GoogleAuth.getAuthUrl(scopes, redirectUri);
 
-  // const authUrl = oauth2Client.generateAuthUrl({
-  //   // 'online' (default) or 'offline' (gets refresh_token)
-  //   access_type: "offline",
-  //   // If you only need one scope you can pass it as a string
-  //   scope: scopes,
-  //   // Enable incremental authorization. Recommended as a best practice.
-  //   include_granted_scopes: true,
-  //   // Consent should be here otherwise google will not return refresh token
-  //   prompt: "consent",
-  // });
   return NextResponse.redirect(authUrl);
 }
--- app/components/credential-tab-list/components/add-calendar.tsx
+++ app/components/credential-tab-list/components/add-calendar.tsx
@@ -1,17 +1,14 @@
+import Link from "next/link";
 import { PlusCircleIcon } from "lucide-react";
 
 export const AddCalendar = () => {
   return (
     <>
-      <form
-        method="GET"
-        action="/api/apps/calendar/add"
-        className="flex justify-center"
-      >
+      <Link href="/api/apps/calendar/add">
         <button type="submit" className="self-center">
           <PlusCircleIcon />
         </button>
-      </form>
+      </Link>
     </>
   );
 };
