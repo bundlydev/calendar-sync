fd76721c17d736235cb7346ad7a590d2e2806131
Alan <alannnc@gmail.com>
2024-03-22 13:36:01 -0700

    trying replace for Link

--- app/components/credential-tab-list/components/add-calendar.tsx
+++ app/components/credential-tab-list/components/add-calendar.tsx
@@ -4,7 +4,7 @@ import { PlusCircleIcon } from "lucide-react";
 export const AddCalendar = () => {
   return (
     <>
-      <Link href="/api/apps/calendar/add">
+      <Link href="/api/apps/calendar/add" replace>
         <button type="submit" className="self-center">
           <PlusCircleIcon />
         </button>
